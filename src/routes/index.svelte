928bff477632a146a152c74f68853d65647d3ed4
GSFgithub-2005 <darrengohsf@gmail.com>
2021-10-17 10:56:48 +0000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<div>
+    <h1 class="text-primary">My profile</h1>
+</div>
+
+<div class="card w-25 mb-2 " style="background-color:#34568B;color:white;">
+    <div class="card-body mt-1 mb-2">
+      <h5 class="card-title font-weight-bold">Details</h5>
+      <p class="card-text">Name : Goh Sheng Fung <br> Age : 16 <br> School : Chung Ling High School</p>
+    </div>
+  </div>
+
+  <div class="card w-25 mt-0" style="background-color:#FF6F61; color:white;">
+    <div class="card-body mt-1 mb-2">
+      <h5 class="card-title font-weight-bold">Hobby</h5>
+      <ul>
+        <li>Coding</li>
+        <li>Work Out</li>
+        <li>Sleeping</li>
+      </ul>
+    
+    </div>
+  </div>
+
+<style>
+    div {
+   	 text-align: center;
+   	 margin: 3em auto;
+    }
+</style>
